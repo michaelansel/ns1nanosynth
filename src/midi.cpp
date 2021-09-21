04ca6a2971cda939c7867d1af2b9ffde84a7ea67
Michael Ansel <git@anselcomputers.com>
2021-09-20 19:55:03 -0700

    Bugfix: use MidiUsb properly

--- src/midi.cpp
+++ src/midi.cpp
@@ -1,11 +1,13 @@
 #include "midi.h"
 
-void midiHook() {
+void midiHook()
+{
     midiEventPacket_t e;
 
-    if (MidiUSB.available() > 0) {
+    e = MidiUSB.read();
+    if (e.header != 0)
+    {
         // Serial.println("new midi message received");
-        e = MidiUSB.read();
         handleMidiEvent(e);
     }
 }
